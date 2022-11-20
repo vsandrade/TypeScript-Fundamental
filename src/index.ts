e37e9141a207b189dcee8d595538c6971dbdee95
Vinicius Andrade <ozirispc@gmail.com>
2022-11-20 16:22:27 -0500

    Seção 01 - Introdução ao TS

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+function component() {
+  const element = document.getElementById('content');
+
+  element.innerHTML = "<h1>Olá Mundo</h1>"
+  element.innerHTML += "<h1>Olá Mundo</h1>"
+  element.innerHTML += "<h1>Olá Mundo</h1>"
+  element.innerHTML += "<h1>Olá Mundo</h1>"
+  element.innerHTML += "<h1>Olá Mundo</h1>"
+  element.innerHTML += "<h1>Olá Mundo</h1>"
+
+  return element;
+}
+
+component();
