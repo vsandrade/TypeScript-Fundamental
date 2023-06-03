3544b1624c71adbdd43325f7da71dd101b9f335f
Vinicius Andrade <ozirispc@gmail.com>
2023-06-03 09:48:34 -0400

    Seção 2 - Variáveis e Tipagens

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,54 @@
-function component() {
-  const element = document.getElementById('content');
+const button = <HTMLInputElement>document.querySelector('button[id="add"]');
+const accessRadio = <HTMLElement>document.getElementById('accessRadio');
+button.addEventListener('click', addEmployee);
 
-  element.innerHTML = "<h1>Olá Mundo</h1>"
-  element.innerHTML += "<h1>Olá Mundo</h1>"
-  element.innerHTML += "<h1>Olá Mundo</h1>"
-  element.innerHTML += "<h1>Olá Mundo</h1>"
-  element.innerHTML += "<h1>Olá Mundo</h1>"
-  element.innerHTML += "<h1>Olá Mundo</h1>"
-
-  return element;
+enum accessOptions {
+  administrator = "administrador",
+  manager = "gerente",
+  employee = "funcionário"
 }
 
-component();
+const accessOptionsValues = Object.values(accessOptions)
+
+accessOptionsValues.forEach((value: string, i: number) => {
+  accessRadio.innerHTML += `
+  <div class="form-check">
+    <input class="form-check-input" type="radio" name="access" id="accessRadio${i}" value="${value}">
+    <label class="form-check-label capitalLetter" for="no">
+      ${value}
+    </label>
+  </div>
+  `
+})
+
+
+function addEmployee(): void {
+  let content = document.getElementById('content') as HTMLInputElement;
+  let fullName: HTMLInputElement | null = document.querySelector('#fullName');
+  let register = <HTMLInputElement>document.querySelector('#register');
+  let admin = document.querySelector('input[name="access"]:checked') as HTMLInputElement;
+  let active = document.querySelector('#active') as HTMLInputElement;
+
+  content.innerHTML += <string>(
+    createLine(fullName!.value, register.value, admin.value, active.checked)
+  );
+}
+
+function createLine(fullName: string, 
+                    NrRegister: string | number, 
+                    admin: string, 
+                    active: boolean): string {
+  return `
+    <div class="card mb-1">
+      <div class="card-header">
+        ${NrRegister}
+      </div>
+      <div class="card-body">
+        <h5 class="card-title">${fullName}</h5>
+        <a href="#" class="btn btn-primary">${active ? 'Ativo' : 'Inativo'}</a>
+      </div>
+      <div class="card-footer bg-transparent border-success">
+        Acesso: ${admin}
+      </div>
+    </div>`;
+}
