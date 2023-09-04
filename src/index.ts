ed6571670da7ae3dca15bc41ac15d9f1a4044314
Vinicius Andrade <ozirispc@gmail.com>
2023-09-04 17:31:15 -0400

    Cleaning code and Creating Async Functions

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,45 @@
+import { accessOptions, userType } from "./models";
+
+let content = document.getElementById('content') as HTMLInputElement;
 const button = <HTMLInputElement>document.querySelector('button[id="add"]');
 const accessRadio = <HTMLElement>document.getElementById('accessRadio');
 button.addEventListener('click', addEmployee);
 
-enum accessOptions {
-  administrator = "administrador",
-  manager = "gerente",
-  employee = "funcionário"
-}
-
 const accessOptionsValues = Object.values(accessOptions)
+
+const getUser = async (): Promise<userType[]> => {
+  const response: Response = await fetch('http://localhost:5011/users');
+  const users: userType[] = await response.json();
+  return users;
+};
+
+const updateUserLayout = async (): Promise<void> => {
+  const users: userType[] = await getUser();
+
+  users.map((user: userType) => {
+    content.innerHTML += <string>createLine(user);
+  });
+};
+
+updateUserLayout()
+
+function addEmployee (): void {
+  let fullName: HTMLInputElement | null = document.querySelector('#fullName');
+  let register = <HTMLInputElement>document.querySelector('#register');
+  let admin = document.querySelector('input[name="access"]:checked') as HTMLInputElement;
+  let active = document.querySelector('#active') as HTMLInputElement;
+
+  let user: userType = {
+    fullName: fullName!.value,
+    register: register.value,
+    access: <accessOptions>admin.value,
+    active: active.checked
+  }
+
+  content.innerHTML += <string>(
+    createLine(user)
+  );
+}
 
 accessOptionsValues.forEach((value: string, i: number) => {
   accessRadio.innerHTML += `
@@ -21,50 +52,18 @@ accessOptionsValues.forEach((value: string, i: number) => {
   `
 })
 
-//Declaration
-function addManager(): void {
-
-}
-
-//Anonymous Function Expression
-const addAdmin = function (): string {
-  return "";
-}
-
-//Arrow Function Expression
-const addMaster = (): never => {
-  while(true) {
-
-  } 
-}
-
-function addEmployee (): void {
-  let content = document.getElementById('content') as HTMLInputElement;
-  let fullName: HTMLInputElement | null = document.querySelector('#fullName');
-  let register = <HTMLInputElement>document.querySelector('#register');
-  let admin = document.querySelector('input[name="access"]:checked') as HTMLInputElement;
-  let active = document.querySelector('#active') as HTMLInputElement;
-
-  content.innerHTML += <string>(
-    createLine(fullName!.value, register.value, admin.value, active.checked)
-  );
-}
-
-function createLine(fullName: string, 
-                    NrRegister: string | number, 
-                    admin: string, 
-                    active: boolean): string {
+function createLine(user: userType): string {
   return `
     <div class="card mb-1">
       <div class="card-header">
-        ${NrRegister}
+        ${user.register}
       </div>
       <div class="card-body">
-        <h5 class="card-title">${fullName}</h5>
-        <a href="#" class="btn btn-primary">${active ? 'Ativo' : 'Inativo'}</a>
+        <h5 class="card-title">${user.fullName}</h5>
+        <a href="#" class="btn btn-primary">${user.active ? 'Ativo' : 'Inativo'}</a>
       </div>
       <div class="card-footer bg-transparent border-success">
-        Acesso: ${admin}
+        Acesso: ${user.access}
       </div>
     </div>`;
 }
---
+++ src/models.ts
@@ -0,0 +1,12 @@
+export enum accessOptions {
+  administrator = 'administrador',
+  manager = 'gerente',
+  employee = 'funcionário',
+}
+
+export type userType = {
+  fullName: string,
+  register: string | number,
+  access: accessOptions,
+  active: boolean
+}
