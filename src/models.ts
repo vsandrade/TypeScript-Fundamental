84346159b5415f8c0668a7d0431c831e180f43af
Vinicius Andrade <ozirispc@gmail.com>
2023-09-08 11:59:04 -0400

    Removing car and working again with user

--- src/models.ts
+++ src/models.ts
@@ -12,46 +12,26 @@ export type userType = {
   active?: boolean
 }
 
-interface ICar {
-  readonly nickname: string;
-  color: string;
-  tires?: number;
-  speedValue: number;
-  breakValue?: number;
-  speed(numero1: number): number;
-  break(numero2: number): number;
+export interface IPerson {
+  fullName: string,
+  register?: string | number,
+  access?: accessOptions,
+  active?: boolean,
+  addPerson(): void;
 }
 
-const carBear: ICar = {
-  nickname: 'The Bear',
-  color: 'Black Piano',
-  speedValue: 5,
-  speed(a: number): number {
-    return this.speedValue;
-  },
-  break(b: number): number {
-    return 2;
-  },
-};
 
-// Eu não posso fazer isso da linha abaixo, porque a propriedade é Read Only. 
-// ou somente leitura.
-// carBear.nickname = 'The Lion'
-console.log(carBear.nickname)
 
-const carOrange: ICar = {
-  nickname: 'The Orange',
-  color: 'Orange',
-  speedValue: 4,
-  breakValue: 3,
-  speed(): number {
-    return this.speedValue;
-  },
-  break: function (): number {
-    return this.breakValue;
-  },
-};
+export interface IUser extends IPerson {
+  addressHome: string,
+  addressWork: string
+}
 
-console.log(carOrange.nickname)
-console.log(carOrange.speed(1))
-console.log(carOrange.break(1))
+const user: IUser = {
+  fullName: 'Vini',
+  addressHome: '29 Casa',
+  addressWork: '25 Trabalho',
+  addPerson(): void {
+    
+  }
+}
