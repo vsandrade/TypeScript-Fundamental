e9df2b23efe0dd962a683bc8381786cae06b8f3e
Vinicius Andrade <ozirispc@gmail.com>
2023-10-03 17:45:53 -0400

    Extends the T generics

--- src/service/service.ts
+++ src/service/service.ts
@@ -1,10 +1,25 @@
-export class GenericService<T> {
+interface genericItem {
+  id: number;
+  name: string;
+}
+
+export class GenericService<T extends genericItem> {
+  public items: T[];
   protected URL: string;
 
-  async getList(): Promise<T[]> {
+  async getItems(): Promise<T[]> {
     const response: Response = await fetch(this.URL);
     const list: T[] = await response.json();
+    this.items = list;
     return list;
+  };
+
+  getItemByName(name: string): T | undefined {
+    return this.items ? this.items.find((item) => (item.name = name)) : undefined
+  };
+
+  getItemById(id: number): T | undefined {
+    return this.items ? this.items.find((item) => (item.id = id)) : undefined
   };
 }
 
