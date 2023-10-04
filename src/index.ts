d7c0c6919db9e1ff737713807d94bb84bf2f5a45
Vinicius Andrade <ozirispc@gmail.com>
2023-10-03 22:49:26 -0400

    Updating commit of generic functions

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
 import UserController from './output';
 
 UserController;
+
+function whatIsIt<T>(arg: T): string {
+  return typeof arg;
+}
+
+console.log(whatIsIt<string>('vini'))
+console.log(whatIsIt<number>(1))
+console.log(whatIsIt<boolean>(true))
